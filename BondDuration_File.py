f90f61d6b97e96e7e12fb68862d76e9487261346
yc4730-WayneChen <yc4730@columbia.edu>
2026-07-04 22:41:44 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
+    adj_y = y/ppy
+    adj_couponRate = couponRate/ppy
+    adj_m = m * ppy
+
+    couponpayment = adj_couponRate * face
+    t = np.arange(1, adj_m+1)
+    wt = np.sum((couponpayment*(1+adj_y)**(-t))*t)+(face*(1+adj_y)**(-adj_m))*(adj_m))
+
+    price = import.BondPrice_File(getBondPrice(y, face, couponRate, m, ppy = 1))
+    Duration = price/wt
+
+    return(Duration)
