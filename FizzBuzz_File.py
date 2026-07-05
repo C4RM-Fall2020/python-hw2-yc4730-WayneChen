0e058ade291dd796b62a594d9fccf756ab0adfc5
yc4730-WayneChen <yc4730@columbia.edu>
2026-07-04 21:55:01 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-   # if start == 0 or finish == 0:<-Optional
-      #raise ValueError("start and finish cannot be 0")<- Optional
+    # if start == 0 or finish == 0:<-Optional
+        #raise ValueError("start and finish cannot be 0")<- Optional
       
-  numvec = np.arange(Start, Finish+1)
-  objvec = np.array(numvec, dtype=object)
-  #if don't use dtype = object, python might treat the item as integer
+    numvec = np.arange(Start, Finish+1)
+    objvec = np.array(numvec, dtype=object)
+    #if don't use dtype = object, python might treat the item as integer
 
-  fizz = (numvec %3 == 0)
-  buzz = (numvec %5 == 0)
+    fizz = (numvec %3 == 0)
+    buzz = (numvec %5 == 0)
 
-  objvec[fizz & buzz] = 'fizzbuzz'
-  objvec[fizz & ~buzz] = 'fizz'
-  objvec[~fizz & buzz] = 'buzz'
+    objvec[fizz & buzz] = 'fizzbuzz'
+    objvec[fizz & ~buzz] = 'fizz'
+    objvec[~fizz & buzz] = 'buzz'
 
-  return objvec
+    return objvec
